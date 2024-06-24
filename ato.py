9871a42ad1ff26caf3b2c93f9a39af7f6d9fd0d0
tvijverb <tgmvijverberg@gmail.com>
2024-06-24 14:29:21 +0200

    solar charger fix gnd connections

--- ato.py
+++ ato.py
@@ -1,7 +1,9 @@
 from atopile import version
+from atopile.components import _get_generic_from_db
 
 def main():
     print(version())
 
 if __name__ == '__main__':
+    _get_generic_from_db("C1234")
     main()
